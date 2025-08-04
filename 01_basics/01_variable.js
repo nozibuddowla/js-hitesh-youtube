ad5d53f9e352b00e31389ac99b0d44c82e4a630a
Md Nozib Ud Dowla <nozibuddowla@gmail.com>
2025-08-04 09:31:26 +0000

    add datatypes file

--- 01_basics/01_variable.js
+++ 01_basics/01_variable.js
@@ -13,7 +13,8 @@ accountCity = "Bengaluru";
 console.log(accountId);
 
 /*
-Prefer not to use var because of issue in block scope and functional scope
+Prefer not to use var 
+because of issue in block scope and functional scope
 */ 
 
 console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,30 @@
+"use strict"; // treat all JS code as newer version
+
+// alert("hello");
+// alert(3 + 3); 
+// We are using node.js, not browser
+
+console.log(3 + 3);
+console.log("Hitesh");
+// code readability should be high
+
+let name = "Hitesh";
+let age = 18;
+let isLoggedIn = false;
+let state;
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined => 
+// symbol => unique
+
+
+// object
+// 
+
+console.log(typeof "hitesh");
+console.log(typeof null); // object
+console.log(typeof undefined); // undefined
