968af0a507055664395bebb09963885a1ccc81b3
Md Nozib Ud Dowla <nozibuddowla@gmail.com>
2025-08-04 10:26:35 +0000

    add datatype coversion

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,34 @@
+let score = "33";
+score = "33abc"
+score = null
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log( valueInNumber);
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+let isLoggedIn = true;
+isLoggedIn = 1; // true
+isLoggedIn = ""; // false
+isLoggedIn = "hitesh"; // true
+
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+
+// console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "hitesh" => true
+
+let someNumber = 33;
+
+let stringNumber = String(someNumber);
+
+console.log(stringNumber);
+console.log(typeof stringNumber);
